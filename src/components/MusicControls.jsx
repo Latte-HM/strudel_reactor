6ad2f94c69bf4058794e226100489acefbda6c64
Latte-HM <lildrdragon@gmail.com>
2025-11-03 23:47:43 +1030

    volume rocker working

--- src/components/MusicControls.jsx
+++ src/components/MusicControls.jsx
@@ -1,18 +1,36 @@
-function MusicControls() {
+function MusicControls({onSlide}) {
     return (
         <>
+            <div className="input-group mb-3">
+                <span className="input-group-text" id="cpm_label">CPM</span>
+                <input type="text" className="form-control" placeholder="e;g 60" aria-label="Username" aria-describedby="basic-addon1"/>
+            </div>
+
+            <label htmlFor="customRange1" className="form-label">Volume Control</label>
+            <input type="range" className="form-range" min="1" max="100" id="volumeControls" onChange={onSlide}></input>
+
             <div className="form-check">
-                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" defaultChecked />
-                <label className="form-check-label" htmlFor="flexRadioDefault1">
-                    p1: ON
+                <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label className="form-check-label" htmlFor="flexCheckDefault">
+                    s1
                 </label>
             </div>
+            
             <div className="form-check">
-                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
-                <label className="form-check-label" htmlFor="flexRadioDefault2">
-                    p1: HUSH
+                <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label className="form-check-label" htmlFor="flexCheckDefault">
+                    d1
                 </label>
             </div>
+
+            <div className="form-check">
+                <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                <label className="form-check-label" htmlFor="flexCheckDefault">
+                    d2
+                </label>
+            </div>
+
+
         </>
     )
 }
